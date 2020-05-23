18953e53a10d4a112514cee758d76f024a5b20b0
Josh Petree <jpgw89@gmail.com>
2020-05-23 12:00:04 -0400

    type:chore
    Configure running app locally with docker and kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -2,17 +2,15 @@
 
 # This tags and uploads an image to Docker Hub
 
-# Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=nirv2389/prediction:1.0
 
-# Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run --image=$dockerpath prediction --port=8000
 
-
-# Step 3:
 # List kubernetes pods
+kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/prediction 8000:80 
 
--- upload_docker.sh
+++ upload_docker.sh
@@ -3,13 +3,11 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
-# Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=nirv2389/prediction:1.0
 
-# Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
-# Step 3:
 # Push image to a docker repository
+docker push nirv2389/prediction:1.0
